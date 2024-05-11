271c294991e7e831ab4765a4fa641fbf2c07b2aa
Vitor José Stähelin <vitorjosestahelin@gmail.com>
2024-05-11 15:51:27 -0300

    fix exchange admin

--- src/api/exchange/admin.py
+++ src/api/exchange/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django import forms
-from api.card.models import CardIllustration
 from api.exchange.models import ExchangeProvider
 from api.exchange.models.card_conection import CardConnection
 
@@ -12,22 +10,16 @@ class ExchangeProviderAdmin(admin.ModelAdmin):
 admin.site.register(ExchangeProvider, ExchangeProviderAdmin)
 
 
-class CardConnectionForm(forms.ModelForm):
-    class Meta:
-        model = CardConnection
-        fields = "__all__"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["card_illustration"].queryset = CardIllustration.objects.all()
-        self.fields["exchange"].queryset = ExchangeProvider.objects.all()
-
-
 class CardConnectionAdmin(admin.ModelAdmin):
     fields = ("card_illustration", "exchange", "external_source_link")
-    list_display = ("card_illustration", "exchange")
+    list_display = ("exchange",)
     list_filter = ("exchange",)
-    form = CardConnectionForm
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ["card_illustration", "exchange"]
+        else:
+            return []
 
 
 admin.site.register(CardConnection, CardConnectionAdmin)
