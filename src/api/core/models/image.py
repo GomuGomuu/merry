c62bd22ed9a8fffb9aad60ea421001ff916af5b2
Vitor José Stähelin <vitorjosestahelin@gmail.com>
2024-10-20 11:34:51 -0300

    feat: set image from url func

--- src/api/core/models/image.py
+++ src/api/core/models/image.py
@@ -1,3 +1,5 @@
+import logging
+import mimetypes
 import uuid
 from datetime import date
 
@@ -5,8 +7,11 @@ from django.core.files.base import ContentFile
 from django.core.files.images import get_image_dimensions
 from django.db import models
 from sorl.thumbnail import ImageField, get_thumbnail
-
 from api.core.utils import slugify
+import requests
+from io import BytesIO
+
+logger = logging.getLogger(__name__)
 
 
 class AbstractImage(models.Model):
@@ -71,11 +76,46 @@ class AbstractImage(models.Model):
 
     def set_image(self, image, file_name=None, save=True):
         image_data = image.read()
-        image_extension = image.content_type.split("/")[-1]
+
+        if hasattr(image, "name"):
+            image_extension = image.name.split(".")[-1].split("?")[0]
+        else:
+            image_extension = "jpg"
+
         file_name = (
-            f"{date.today()}-{uuid.uuid4()}-{slugify(image.src.name[:-(len(image_extension) + 1)])}.{image_extension}"
+            f"{date.today()}-{uuid.uuid4()}-{slugify(file_name)}.{image_extension}"
             if file_name
             else f"{date.today()}-{uuid.uuid4()}-unnamed.{image_extension}"
         )
+
         src = ContentFile(image_data)
         self.src.save(file_name, src, save=save)
+
+    def set_image_from_url(self, url, save=True):
+        logger.debug(f"Downloading image from {url}")
+
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+
+            content_type = response.headers.get("Content-Type")
+            if not content_type or "image" not in content_type:
+                raise ValueError(f"URL does not point to an image: {url}")
+
+            image_content = BytesIO(response.content)
+            image_name = url.split("/")[-1]
+
+            image_extension = mimetypes.guess_extension(content_type.split(";")[0])
+
+            if not image_extension:
+                image_extension = (
+                    image_name.split(".")[-1] if "." in image_name else "jpg"
+                )
+
+            image = ContentFile(image_content.read(), name=image_name)
+            self.set_image(image, file_name=image_name, save=save)
+            logger.debug(f"Image downloaded and set from {url}")
+
+        except Exception as e:
+            logger.error(f"Error downloading image from {url}: {e}")
+            raise e
